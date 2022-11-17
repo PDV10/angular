b5d9f47923109ca00c7ec3feb849d23a37c61063
pato <patooodv@gmail.com>
2022-11-17 15:53:35 -0300

    se agregan mas filtros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Cubos } from './cubos/Cubos';
+import { DataCubosService } from './data-cubos.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'Cubos Olav';
 }
